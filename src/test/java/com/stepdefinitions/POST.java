30bfeb61530f8e0ca136df7bdb9a0966a88c79ec
AdanMerino-AT <brayan.merino@agilethought.com>
2020-11-03 13:33:14 -0600

    Create POST feature file and stepdefinitios config

---
+++ src/test/java/com/stepdefinitions/POST.java
@@ -0,0 +1,86 @@
+package com.stepdefinitions;
+
+import cucumber.api.java.en.Then;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class POST {
+	@Given("I have a story name")
+	public void i_have_a_story_name() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I have a story description")
+	public void i_have_a_story_description() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I have acceptance criteria")
+	public void i_have_acceptance_criteria() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I have story points asigned")
+	public void i_have_story_points_asigned() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I have a progress")
+	public void i_have_a_progress() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I have the start date")
+	public void i_have_the_start_date() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I have the due date")
+	public void i_have_the_due_date() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I have the create date")
+	public void i_have_the_create_date() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I have a status")
+	public void i_have_a_status() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("i prepare the resource")
+	public void i_prepare_the_resource() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I create a new story on the api")
+	public void i_create_a_new_story_on_the_api() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The API should indicate that the story was created with a {int} status code")
+	public void the_API_should_indicate_that_the_story_was_created_with_a_status_code(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("i verify in the database if the story exist")
+	public void i_verify_in_the_database_if_the_story_exist() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
